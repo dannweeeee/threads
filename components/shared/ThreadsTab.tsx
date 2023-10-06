1e13abdb7e44d2465a621f3d5ace833432044a6d
Dann Wee <dannweezj@gmail.com>
2023-10-06 22:42:41 +0700

    profile threads count and threads list

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType } : Props) => {
+    // TODO: Fetch Profile Threads
+
+    return (
+        <section>
+            ThreadsTab
+        </section>
+    )
+}
+
+export default ThreadsTab;
