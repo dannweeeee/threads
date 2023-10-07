a1fa5432d4c2b4cbc9cda281ca6fc155d2bbb6ac
Dann Wee <dannweezj@gmail.com>
2023-10-07 11:26:13 +0700

    usercard

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -57,31 +57,31 @@ function PostThread({ userId }: { userId: string }) {
     };
 
     return (
-        <Form {...form}>
-          <form
-            className='mt-10 flex flex-col justify-start gap-10'
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <FormField
-              control={form.control}
-              name='thread'
-              render={({ field }) => (
-                <FormItem className='flex w-full flex-col gap-3'>
-                  <FormLabel className='text-base-semibold text-light-2'>
-                    Content
-                  </FormLabel>
-                  <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
-                    <Textarea rows={15} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type='submit' className='bg-primary-500'>
-              Post Thread
-            </Button>
-          </form>
-        </Form>
+      <Form {...form}>
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Content
+              </FormLabel>
+              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='bg-primary-500'>
+          Post Thread
+        </Button>
+      </form>
+    </Form>
     );
 }
 
