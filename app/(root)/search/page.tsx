5ea2be9a13e1ad46a7b74abaf32aa49f215ab8a5
Dann Wee <dannweezj@gmail.com>
2023-10-08 22:25:17 +0700

    searchbar fix and utfs error handle

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,57 +1,62 @@
-import ProfileHeader from '@/components/shared/ProfileHeader';
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
-import { currentUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
 
-import Image from 'next/image';
-import { profileTabs } from '@/constants';
-import ThreadsTab from '@/components/shared/ThreadsTab';
-import { resourceLimits } from 'worker_threads';
-import UserCard from '@/components/cards/UserCard';
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-async function Page() {
-    const user = await currentUser();
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-    if(!user) return null;
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-    const userInfo = await fetchUser(user.id);
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-    if (!userInfo?.onboarded) redirect('/onboarding'); // move all the users that maybe switch their url bar manually, it will bring them back to onboarding
-
-    // Fetch Users
-    const result = await fetchUsers({
-        userId: user.id,
-        searchString: '',
-        pageNumber: 1,
-        pageSize: 25,
-    })
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
   return (
     <section>
-        <h1 className="head-text mb-10">Search</h1>
+      <h1 className='head-text mb-10'>Search</h1>
 
-        { /* Search Bar */ }
+      <Searchbar routeType='search' />
 
-        <div className="mt-14 flex flex-col gap-9">
-            {result.users.length === 0 ? (
-                <p className="no-result">No Users</p>
-            ) : (
-                <>
-                    {result.users.map((person) => (
-                        <UserCard 
-                            key={person.id}
-                            id={person.id}
-                            name={person.name}
-                            username={person.username}
-                            imgUrl={person.image}
-                            personType="User"
-                        />
-                    ))}
-                </>
-            )}
-        </div>
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </section>
-  )
+  );
 }
 
-export default Page
+export default Page;
