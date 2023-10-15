dd3aaddadb4f4b1ecfab629b364f20fbfbd52369
Dann Wee <dannweezj@gmail.com>
2023-10-15 22:27:36 +0700

    update threadcard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -86,7 +86,26 @@ const ThreadCard = ({
                     isComment={isComment}
                 />
                 
-                { /* TODO: Show Comment Logos */}
+                {!isComment && comments.length > 0 && (
+                    <div className='ml-1 mt-3 flex items-center gap-2'>
+                    {comments.slice(0, 2).map((comment, index) => (
+                        <Image
+                        key={index}
+                        src={comment.author.image}
+                        alt={`user_${index}`}
+                        width={24}
+                        height={24}
+                        className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+                        />
+                    ))}
+
+                    <Link href={`/thread/${id}`}>
+                        <p className='mt-1 text-subtle-medium text-gray-1'>
+                        {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                        </p>
+                    </Link>
+                    </div>
+                )}
 
             </div>
                 {!isComment && community && (
